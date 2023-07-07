618beb16946ae32ff20119e73be1e32f44fa7fa1
Ben Deverett <deverett@rice09.stanford.edu>
2023-07-06 17:55:31 -0700

    cluster

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,10 +1,16 @@
 ##
+# convert *.png mand.gif
 # convert mand.gif -coalesce -duplicate 1,-2-1 -quiet -layers OptimizePlus -loop 0 mandloop.gif
 
 from multiprocessing import Process
+import logging
 import numpy as np
 import matplotlib.pyplot as pl
-from soup.animation import Animation
+pl.ioff()
+import os
+
+logging.basicConfig(filename='log.log', level=logging.DEBUG, format='%(name)s - %(levelname)s - %(message)s')
+logging.getLogger('matplotlib.font_manager').disabled = True
 
 def mandelbrot(x, y, threshold):
     """Calculates whether the number c = x + i*y belongs to the 
@@ -27,7 +33,7 @@ def mandelbrot(x, y, threshold):
         
     return threshold - 1  # it didn't diverge
 
-def draw_img(center, wh, thresh, npix=200, arr=None):
+def generate_img(center, wh, thresh, npix=200, arr=None):
     re0, im0 = center
     w = h = wh
 
@@ -43,47 +49,52 @@ def draw_img(center, wh, thresh, npix=200, arr=None):
             arr[i, j] = mandelbrot(R, I, thresh)
     return arr, (im, re)
 
-#center=(-1.2963247639177446, 0.44182080996569695)
-#center = (-0.46475777464800705, -0.5450620084753436)
-
+# Params
 thresh = 800
 npix = 800
-zoom_init = 2
-center = (-1.2500579833374386, 0.00549374971639973)
-im, coords = draw_img(center=center,
-                      wh=zoom_init, #w/h in complex plane coords
-                      thresh=thresh,
-                      npix=npix,
-                      arr=None)
-y0, y1, x0, x1 = coords[0][0], coords[0][-1], coords[1][0], coords[1][-1]
-
-fig = pl.figure(figsize=(6,6))
-ax = fig.add_axes([0, 0, 1, 1])
-img = ax.imshow(im[::-1], cmap=pl.cm.inferno_r,
-                extent=(x0, x1, y0, y1))
-
-##
 zooms = np.logspace(0.3, -15, 150)
-an = Animation('mandel', fig=fig, rate=5.0)
-for zoom in zooms:
-    print(zoom)
-    im, coords = draw_img(center=center,
-                          wh=zoom,
-                          arr=im,
+zooms = np.array([zooms, np.arange(len(zooms))]).T
+#center=(-1.2963247639177446, 0.44182080996569695)
+#center = (-0.46475777464800705, -0.5450620084753436)
+center = (-1.2500579833374386, 0.00549374971639973)
+save_dir = f'imgs/{center[0]}-{center[1]}_{zooms[0,0]}-{zooms[-1,0]}_{thresh}_{npix}'
+os.makedirs(save_dir)
+n_procs = 8
+
+def display_images(zooms, center, thresh, npix, cmap=pl.cm.magma_r):
+    # create a baseline image object
+    im, coords = generate_img(center=center,
+                          wh=zooms[0,0], #w/h in complex plane coords
                           thresh=thresh,
-                          npix=npix)
-    img.set_data(im[::-1])
-    img.set_clim(im.min(), im.max())
-    an.put()
-#an.end()
+                          npix=npix,
+                          arr=None)
+    y0, y1, x0, x1 = coords[0][0], coords[0][-1], coords[1][0], coords[1][-1]
 
-proc = Process(target=print_func, args=(name,))
-procs.append(proc)
-                proc.start()
+    fig = pl.figure(figsize=(6,6))
+    ax = fig.add_axes([0, 0, 1, 1])
+    img = ax.imshow(im[::-1], cmap=cmap,
+                    extent=(x0, x1, y0, y1))
 
-                    # complete the processes
-                        for proc in procs:
-                                    proc.join()
+    for zoom, zoom_id in zooms:
+        zoom_id = int(zoom_id)
+        logging.info(f'Running {zoom_id} = {zoom} (batch {zooms[0,1]}-{zooms[-1,1]})')
+        im, coords = generate_img(center=center,
+                              wh=zoom,
+                              arr=im,
+                              thresh=thresh,
+                              npix=npix)
+        img.set_data(im[::-1])
+        img.set_clim(im.min(), im.max())
+        fig.savefig(os.path.join(save_dir, f'{zoom_id:05}.png'))
 
+zlists = np.array_split(zooms, n_procs, axis=0)
+procs = []
+for i, zlist in enumerate(zlists):
+    proc = Process(target=display_images, args=(zlist, center, thresh, npix))
+    procs.append(proc)
+    proc.start()
+
+for proc in procs:
+    proc.join()
 
 ##
---
+++ submit.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#SBATCH -N 1      # nodes requested
+#SBATCH -n 2      # tasks requested
+#SBATCH -c 8      # cores requested
+#SBATCH --mem=1000  # memory in Mb
+#SBATCH -o log.log  # send stdout to outfile
+#SBATCH -e log.log  # send stderr to errfile
+#SBATCH -t 2:00:00  # time requested in hour:minute:second
+
+module load anaconda3/2019.10
+source activate py39
+python mandelbrot.py
